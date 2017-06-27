56fd435b2d4561030505b242d5a961d87735440e
Max Bremer <bremerm31@gmail.com>
2017-06-27 11:11:33 -0500

    Addding partition class

---
+++ src/partition.h
@@ -0,0 +1,45 @@
+#ifndef PARTITION_H
+#define PARTITION_H
+
+#include <hpx/hpx_main.hpp>
+#include <hpx/include/iostreams.hpp>
+
+#include <vector>
+
+class Partition {
+public:
+
+  Partition() = default;
+  Partition(std::size_t id, std::size_t grain_size=10)
+    : _id(id), _t(0), _my_vector(grain_size) {}
+
+  void perform_one_timestep {
+
+    hpx::cout << "Performing one timestep on " << id << " @ " << t++ << "\n";
+
+  }
+
+  void send {
+
+    hpx::cout << "Doing send on " << id << " @ " << t << "\n";
+    
+  }
+
+
+  void receive {
+
+    hpx::cout << "Doing receive on " << id << " @ " << t << "\n";
+    
+  }
+
+
+private:
+
+  std::size_t id;
+  std::uint64_t t;
+
+  std::vector<double> _my_vector;
+
+};
+
+#endif
