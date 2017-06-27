b2302338f376daf53e18fa84702801b759809d75
Max Bremer <bremerm31@gmail.com>
2017-06-27 11:28:03 -0500

    Compiles now

--- src/partition.h
+++ src/partition.h
@@ -1,6 +1,8 @@
 #ifndef PARTITION_H
 #define PARTITION_H
 
+
+//#include <iostream>
 #include <hpx/hpx_main.hpp>
 #include <hpx/include/iostreams.hpp>
 
@@ -13,22 +15,22 @@ public:
   Partition(std::size_t id, std::size_t grain_size=10)
     : _id(id), _t(0), _my_vector(grain_size) {}
 
-  void perform_one_timestep {
+  void perform_one_timestep() {
 
-    hpx::cout << "Performing one timestep on " << id << " @ " << t++ << "\n";
+    hpx::cout << "Performing one timestep on " << _id << " @ " << _t++ << "\n";
 
   }
 
-  void send {
+  void send() {
 
-    hpx::cout << "Doing send on " << id << " @ " << t << "\n";
+    hpx::cout << "Doing send on " << _id << " @ " << _t << "\n";
     
   }
 
 
-  void receive {
+  void receive() {
 
-    hpx::cout << "Doing receive on " << id << " @ " << t << "\n";
+    hpx::cout << "Doing receive on " << _id << " @ " << _t << "\n";
     
   }
 
--- src/stepper.h
+++ src/stepper.h
@@ -1,18 +1,20 @@
 #ifndef STEPPER_H
 #define STEPPER_H
 
+#include <hpx/hpx.hpp>
+
 #include <vector>
+
+#include "partition.h"
 
 //typedef hpx::serialization::serialize_buffer<double> hpx_serialize_buffer;
 
-class stepper{
+class Stepper{
 
   // Initialize partitions
-  void initialize(std::size_t steps); // in stepper/initialize.cpp
+  hpx::future<void> run(std::size_t steps); // in stepper/initialize.cpp
 
-  // Update step for a single partition
-  hpx::future<void> partition::perform_one_time_step(int t);
+  std::vector<Partition> my_partitions;
 
-
-}
+};
 #endif
