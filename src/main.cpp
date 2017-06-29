32d0a9c5932e5657ecbf2072c7c0d9a6915667f4
Max Bremer <bremerm31@gmail.com>
2017-06-29 14:50:27 -0500

    X

--- src/main.cpp
+++ src/main.cpp
@@ -17,10 +17,6 @@ int hpx_main(int argc, char **argv) {
   Stepper myStepper;
   hpx::future<void> f = myStepper.run(timesteps);
 
-  hpx::lcos::channel<double> buffer(hpx::find_here());
-  std::string const channel_string = "channel!";
-  buffer.register_as(channel_string);
-
   f.get();
 
   return hpx::finalize();
--- src/stepper/perform_one_timestep.cpp
+++ src/stepper/perform_one_timestep.cpp
@@ -5,11 +5,12 @@ hpx::future<void> Partition::perform_one_timestep()
 
   hpx::future<void> send_future = outgoing.set(hpx::launch::async, _t);
 
-  hpx::future<void> work_future = hpx::async( &(this->work()) );
+  hpx::future<void> work_future = hpx::async( &Partition::work, this );
 
   hpx::future<void> receive_future = incoming.get(_t).then(
-				      [this](hpx::future<double> f)
-				        this->receive( f.get() );
+				        [this](hpx::future<double> f) {
+				          this->receive( f.get() );
+					}
 				      );
 
   //hpx::future< std::tuple<hpx::future<void>, hpx::future<void>, hpx::future<void> > >
