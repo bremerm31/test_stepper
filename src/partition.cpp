4caed88ca1aa0d801f096687bf80f55ccea61422
Craig Michoski <michoski@c557-203.stampede.tacc.utexas.edu>
2017-06-30 10:44:32 -0500

    Broke things -- cem

--- src/partition.cpp
+++ src/partition.cpp
@@ -13,17 +13,17 @@ hpx::future<void> Partition::perform_one_timestep()
 
   std::cout << "In prf1ts\n";
 
-  //hpx::future<void> send_future = outgoing.set(hpx::launch::async, _id, _t);
-
   hpx::future<void> work_future = hpx::async( &Partition::work, this );
+  outgoing.set(hpx::launch::sync, static_cast<double>(_id));
+  //  hpx::future<void> send_future = outgoing.set(hpx::launch::sync, static_cast<double>(_id));
 
-  // hpx::future<void> receive_future = incoming.get(_t).then(
-  // 				        [this](hpx::future<double> f) {
-  // 				          this->receive( f.get() );
-  // 					}
-  //				      );
+  hpx::future<void> receive_future = incoming.get().then(
+  				        [this](hpx::future<double> f) {
+  				          this->receive( f.get() );
+  					}
+  				      );
 
-//hpx::future< std::tuple<hpx::future<void>, hpx::future<void>, hpx::future<void> > >
-return work_future;  
-//return hpx::when_all( send_future, work_future, receive_future );
+  //hpx::future< std::tuple<hpx::future<void>, hpx::future<void>, hpx::future<void> > >
+  //return work_future;  
+  return hpx::when_all( work_future, receive_future );
 }
