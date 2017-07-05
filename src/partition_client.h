acfeb5a34a1980fae131a074e1af9e88b857601f
Max Bremer <bremer@login1.stampede2.tacc.utexas.edu>
2017-07-05 13:12:11 -0500

    X

--- src/partition_client.h
+++ src/partition_client.h
@@ -8,11 +8,12 @@ struct PartitionClient : hpx::components::client_base<PartitionClient, Partition
   using base_type = hpx::components::client_base<PartitionClient, Partition>;
 
 
-  PartitionClient(hpx::future<hpx::id_type>&& id)
-    : base_type(std::move(id))
+  PartitionClient(hpx::id_type where, std::size_t id, std::size_t n_ids)
+    : base_type(hpx::new_<Partition>(where, id, n_ids) )
   {}
 
-  hpx::future<void> perform_one_timestep() {
+  hpx::future<void> perform_one_timestep()
+  {
     Partition::perf_action act;
 
     hpx::cout << "in partition client\n";
