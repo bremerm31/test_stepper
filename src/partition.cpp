2fbdcee659acdb705826227960a4458364b6222f
Max Bremer <bremer@login2.stampede2.tacc.utexas.edu>
2017-07-06 10:55:27 -0500

    Adding vectors of channels

--- src/partition.cpp
+++ src/partition.cpp
@@ -13,20 +13,26 @@ hpx::future<void> Partition::perform_one_timestep()
 
   std::cout << "In prf1ts\n";
 
-  hpx::future<void> send_future = hpx::async( &Partition::send, this );
+  std::vector<hpx::future<void> > send_futures;
+  send_futures.reserve( outgoing.size() );
+  send_futures.push_back( hpx::async( &Partition::send, this, 0 ) );
+  send_futures.push_back( hpx::async( &Partition::send, this, 1 ) );
 
   hpx::future<void> work_future = hpx::async( &Partition::work, this );
 
-  hpx::future<void> receive_future = incoming.get(_t).then(
-  				        [this](hpx::future<double> f) {
-  				          this->receive( f.get() );
-  					}
-					);
+  std::vector<hpx::future<void> > receive_futures;
+  for ( uint indx = 0; indx < 2; ++indx ) {
+    hpx::future<void> receive_future = incoming[indx].get(_t).then(
+							     [this](hpx::future<double> f) {
+							       this->receive( f.get() );
+							     }
+							     );
+  }
 
   //hpx::future< std::tuple<hpx::future<void>, hpx::future<void>, hpx::future<void> > >
   //return work_future;  
-  return hpx::when_all( send_future, work_future, receive_future ).then(
-									[this](auto&& f) {
-									  this->update();
-									});
+  return hpx::when_all( send_futures, work_future, receive_futures ).then(
+									  [this](auto&& f) {
+									    this->update();
+									  });
 }
--- src/partition.h
+++ src/partition.h
@@ -12,48 +12,69 @@ struct Partition : public hpx::components::simple_component_base<Partition>
 {
   Partition() : _id(1000) {}
   Partition(std::size_t id, std::size_t n_ids)
-    : _id(id), _t(0), n_ids(n_ids), _my_value(id), incoming(hpx::find_here())
+    : _id(id), _t(0), n_ids(n_ids), _my_value(id)
   {
     hpx::cout << " In Partition ctor " << id << " of " << n_ids << "\n";
 
-    std::string const in_channel_string = "channel"+std::to_string(id);
-    hpx::future<void> set_up_in = incoming.register_as(in_channel_string);
 
-    //connect to the guy on the left
-    std::string const out_channel_string = "channel"+std::to_string( ( id + 1 ) % n_ids);
-    outgoing.connect_to(out_channel_string);
+    //The convention is that the first channel number is the send submesh and the second number is the receiving submesh 
+    std::vector<hpx::future<void> > set_up_in;
+    {
+      incoming.push_back( hpx::lcos::channel<double>(hpx::find_here() ) );
+      std::string const in_channel_string = "channel"+std::to_string(id)+"_"+std::to_string(( id + 1 ) % n_ids);
+      set_up_in.push_back(incoming[0].register_as(in_channel_string) );
+    }
+    {
+      incoming.push_back( hpx::lcos::channel<double>(hpx::find_here() ) );
+      std::string const in_channel_string = "channel"+std::to_string(id)+"_"+std::to_string((id +n_ids -1) % n_ids);
+      set_up_in.push_back(incoming[1].register_as(in_channel_string) );
+    }
 
 
-    //set_up_in.get();
+    {
+      outgoing.push_back( hpx::lcos::channel<double>() );
+      std::string const out_channel_string = "channel"+std::to_string(( id + 1 ) % n_ids)+"_"+std::to_string(id);
+      outgoing[0].connect_to(out_channel_string);
+    }
+    {
+      outgoing.push_back( hpx::lcos::channel<double>() );
+      std::string const out_channel_string = "channel"+std::to_string((id +n_ids -1) % n_ids)+"_"+std::to_string(id);
+      outgoing[1].connect_to(out_channel_string);
+    }
+
+    hpx::when_all(set_up_in).get();
   }
 
   hpx::future<void> perform_one_timestep();
 
-
+  inline
   void work() {
 
     hpx::cout << "Performing one timestep on " << _id << " @ " << _t << "\n";
 
   }
 
-  void send() {
+  inline
+  void send(uint indx) {
 
     hpx::cout << "Doing send on " << _id << " @ " << _t << "\n";
-    outgoing.set(static_cast<double>(_id), _t);
+    outgoing[indx].set(static_cast<double>(_id), _t);
   }
 
+  inline
   void receive( double msg ) {
 
     hpx::cout << "Received msg: " << msg << " on " << _id << " @ " << _t << "\n";
   }
 
+  inline
   void update()
   { ++_t; };
 
   HPX_DEFINE_COMPONENT_ACTION(Partition, perform_one_timestep, perf_action);
 
-  hpx::lcos::channel<double> incoming;
-  hpx::lcos::channel<double> outgoing;
+  std::vector<hpx::lcos::channel<double> > incoming;
+  std::vector<hpx::lcos::channel<double> > outgoing;
 
   std::size_t _id;
   std::uint64_t _t;
